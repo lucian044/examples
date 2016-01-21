db69e5a3028677fda4d3587e86a32f6af454c8aa
Anthony Deeter <aed27@zips.uakron.edu>
2016-01-21 16:05:47 -0500

    added sample example file

---
+++ sample/sample.cpp
@@ -0,0 +1,8 @@
+// This is a sample example, in the examples repo!
+// you can clone this directory from github and run it on your own machine!
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+  std::cout << "This is a sample example!" <<std::endl;
+}
