2e6ed3fa61f15cf8a7d04b08ed538d440857eb66
Anthony Deeter <aed27@zips.uakron.edu>
2016-01-28 16:33:33 -0500

    Fixed the wrong terminology for definition

--- ch11/basic_struct/basic_struct.cpp
+++ ch11/basic_struct/basic_struct.cpp
@@ -1,5 +1,5 @@
 // Contains a basic structure
-// Declaration and Definition of a structure
+// Definition of a structure
 // Accessing member variables
 // Outputing member variables
 // The dot operator
@@ -8,9 +8,9 @@
 #include <iostream>
 #include <string>
 
-// The declaration of a structure named 'Student' 
+// The definition of a structure named 'Student'
 // The name of the struct is also called the tag
-// The declaration tells the compiler what the structure is composed of
+// The definition tells the compiler what the structure is composed of
 struct Student {
 
   // each member variable of the structure is declared using the data type and a name
@@ -22,20 +22,20 @@ struct Student {
 }; // do not forget this semi colon, ever!
 
 int main() {
-  
-  // Defining an instance of the Student data type
+
+  // Creating an instance of the Student data type
   // It is important to remember the member variables are not initialized
   // The instance, named 'st', has its own copy of each of the member variables
   Student st;
- 
+
   // the dot operator . is used to access structure members
   // st.age will access the age member of the instance of Student named 'st'
-  
+
   // output the Student information using the dot operator before initialization
   std::cout << "The age of our student is " << st.age << "." << std::endl;
   std::cout << "The name of our student is " << st.name << "." << std::endl;
   std::cout << "The ID of our student is " << st.ID << "." << std::endl;
-  
+
   std::cout << "------------ after initilization ------------ " << std::endl;
 
   // Initialize the member variables for the st variable using the dot operator
@@ -47,8 +47,5 @@ int main() {
   // output the Student information using the dot operator after initialization
   std::cout << "The age of our student is " << st.age << "." << std::endl;
   std::cout << "The name of our student is " << st.name << "." << std::endl;
-  std::cout << "The ID of our student is " << st.ID << "." << std::endl;  
+  std::cout << "The ID of our student is " << st.ID << "." << std::endl;
 }
-
-
-
